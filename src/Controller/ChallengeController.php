eb7589807d14adb7ad0458bd22f91f0ce6210db4
LATIBI adel <adel.latibi@gmail.com>
2022-11-16 09:43:03 +0100

    correction tp challngeur

--- src/Controller/ChallengeController.php
+++ src/Controller/ChallengeController.php
@@ -2,23 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\Challenge;
+use App\Repository\ChallengeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class ChallengeController extends AbstractController{
 
+    private $challengeRepository;
 
-    public function detail():Response{
-        return $this->render("challenge/detail.html.twig");
+    public function __construct(ChallengeRepository $challengeRepository)
+    {
+        $this->challengeRepository = $challengeRepository;
+    }
+
+    public function detail(Challenge $challenge ):Response{
+        return $this->render("challenge/detail.html.twig",compact("challenge"));
     }
 
 
     public function add():Response{
+        $challenge = (new Challenge)->setNom("test 2")
+                                    ->setDescription("blablabla")
+                                    ->setNbParticpants(12)
+                                    ->setDateHeure(new \DateTime())
+                                    ->setCreateur("Auteur 2");
+        $this->challengeRepository->add($challenge,true);
+        
         return $this->render("challenge/add.html.twig");
     }
 
-
     public function list():Response{
-        return $this->render("challenge/list.html.twig");
+        $challenges = $this->challengeRepository->findAll();
+        return $this->render("challenge/list.html.twig",compact("challenges"));
     }
 }
---
+++ src/Entity/Challenge.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ChallengeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ChallengeRepository::class)
+ */
+class Challenge
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=500, nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbParticpants;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateHeure;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $createur;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getNbParticpants(): ?int
+    {
+        return $this->nbParticpants;
+    }
+
+    public function setNbParticpants(int $nbParticpants): self
+    {
+        $this->nbParticpants = $nbParticpants;
+
+        return $this;
+    }
+
+    public function getDateHeure(): ?\DateTimeInterface
+    {
+        return $this->dateHeure;
+    }
+
+    public function setDateHeure(\DateTimeInterface $dateHeure): self
+    {
+        $this->dateHeure = $dateHeure;
+
+        return $this;
+    }
+
+    public function getCreateur(): ?string
+    {
+        return $this->createur;
+    }
+
+    public function setCreateur(string $createur): self
+    {
+        $this->createur = $createur;
+
+        return $this;
+    }
+}
